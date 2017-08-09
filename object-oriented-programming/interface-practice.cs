cf8d7f2d652a026f2a60ae744efa1a1b4ff6351e
Matt <matt.paauw@gmail.com>
2017-08-09 08:34:37 -0700

    Added new interface practice program.

---
+++ object-oriented-programming/interface-practice.cs
@@ -0,0 +1,45 @@
+static void Main(string[] args)
+{
+    Bible bible = new Bible(true, 9);
+    System.Diagnostics.Debug.WriteLine(bible.timeToReadInSeconds(2000, 120, 8));
+
+    Brochure brochure = new Brochure(5);
+    System.Diagnostics.Debug.WriteLine(brochure.timeToReadInSeconds(5, 30, 14));
+}
+
+public class Bible : Book
+{
+    private bool isTranslated;
+    private int complexityFactor;
+
+    public Bible(bool isTranslated, int complexityFactor)
+    {
+        this.isTranslated = isTranslated;
+        this.complexityFactor = complexityFactor;
+    }
+
+    public double timeToReadInSeconds(int numPages, double secondsPerPage, int fontSize)
+    {
+        return (isTranslated) ? (numPages * secondsPerPage) + (secondsPerPage / fontSize) * complexityFactor : int.MaxValue;
+    }
+}
+
+public class Brochure : Book
+{
+    private int numIllustrations;
+
+    public Brochure(int numIllustrations)
+    {
+        this.numIllustrations = numIllustrations;
+    }
+
+    public double timeToReadInSeconds(int numPages, double secondsPerPage, int fontSize)
+    {
+        return (numPages * secondsPerPage) + (secondsPerPage / fontSize) - (numIllustrations * secondsPerPage);
+    }
+}
+
+public interface Book
+{
+    double timeToReadInSeconds(int numPages, double secondsPerPage, int fontSize);
+}
