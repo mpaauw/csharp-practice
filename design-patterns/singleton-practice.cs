ca161eee1abfd8346c2f4c55e89ed9d4e34673b0
Matt <matt.paauw@gmail.com>
2017-08-08 21:00:24 -0700

    Modified singleton-practice implementation.

--- design-patterns/singleton-practice.cs
+++ design-patterns/singleton-practice.cs
@@ -1,14 +1,36 @@
-public class Singleton {
+static void Main(string[] args)
+{
+    Singleton singleton = Singleton.getInstance();
+    singleton.testInstance();
+    singleton = Singleton.getInstance();
+    Singleton singletonSequel = Singleton.getInstance();
+}
 
+public class Singleton
+{
     private static Singleton instance = null;
 
-    private Singleton() {
+    private Singleton()
+    {
         // cannot be instantiated
     }
 
-    public static Singleton getInstance() {
-        if(this.instance == null) {
-            return this.instance.getInstance();
+    public static Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+            System.Diagnostics.Debug.WriteLine("Singleton created.");
         }
+        else
+        {
+            System.Diagnostics.Debug.WriteLine("Unable to get instance, one already exists.");
+        }
+        return instance;
+    }
+
+    public void testInstance()
+    {
+        System.Diagnostics.Debug.WriteLine("Singleton valid.");
     }
 }
